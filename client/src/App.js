4f424ccbc391291cfc8b39f6c0b3b080b1599720
githyin <kmkm9460@gmail.com>
2023-12-20 20:15:04 +0900

    i don't know

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import ChatScreen from "./component/ChatScreen/ChatScreen";
 import FileUpload from "./component/FileUpload/FileUpload";
 import CamScreen from "./component/CamScreen/CamScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <ChatScreen />
-      <FileUpload />
-      <CamScreen />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ChatScreen />} />
+        <Route path="/fileupload" element={<FileUpload />} />
+        <Route path="/camscreen" element={<CamScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
