676f486e9c2d07be5ef058f53f6a2aa82a45bf11
gweng0407 <152453133+gweng0407@users.noreply.github.com>
2023-12-20 20:49:03 +0900

    Client_ChatScreen

--- client/src/component/ChatScreen/ChatScreen.js
+++ client/src/component/ChatScreen/ChatScreen.js
@@ -1,29 +1,56 @@
 // ChatScreen.js
-import React from "react";
-import ChatMessage from "./ChatMessage";
-import styles from "../../css/ChatScreen/ChatScreen.module.css";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import ChatMessage from './ChatMessage';
+import styles from '../../css/ChatScreen/ChatScreen.module.css';
 
-export default function ChatScreen() {
+function Chat() {
+  const [messageText, setMessageText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const ws = new WebSocket('ws://localhost:8000/ws');
+
+  useEffect(() => {
+    ws.onmessage = function(event) {
+      setMessages(prevMessages => [...prevMessages, event.data]);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    ws.send(messageText);
+    setMessageText('');
+  };
+
   return (
     <div className={styles.chatScreen}>
       <div className={styles.chatHeader}>
         <h2 className="text-2xl font-semibold text-center mb-4">Chat Screen</h2>
-        <div className={styles.chatMessages}>
-          <div className="flex flex-col gap-4">
-            <ChatMessage isSentByUser={false} text="Hello, how are you?" />
-            <ChatMessage isSentByUser={true} text="I'm fine, thank you!" />
-            {/* Add more messages here */}
+          <div className={styles.chatMessages}>
+            <div className="flex flex-col gap-4">
+              {messages.map((message, index) => (
+                <ChatMessage key={index} isSentByUser={false} text={message} />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={styles.messageInput}>
+          <div className={styles.messageInput}>
           <input
             className="flex-1"
             placeholder="Type a message..."
             type="text"
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+            autoComplete="off"
           />
-          <button>Send</button>
+          <button type="submit" onClick={sendMessage}>Send</button>
         </div>
       </div>
     </div>
   );
 }
+
+export default Chat;
