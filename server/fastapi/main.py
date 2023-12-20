e46870d6c90b2301178732e434c391a8372fdbe5
gweng0407 <152453133+gweng0407@users.noreply.github.com>
2023-12-20 20:19:42 +0900

    hmm

--- server/fastapi/main.py
+++ server/fastapi/main.py
@@ -1,26 +1,8 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-
-# CORS 미들웨어 추가
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React 앱이 실행되는 주소
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+from fastapi import FastAPI
+from fastapi_socketio import SocketManager
+ 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-
-# 동적 데이터 반환 함수 (가정)
-def get_dynamic_data():
-    return {"video_title": "Sample Video", "view_count": 1000, "comments": ["Great!", "Awesome!"]}
-
-# 루트 경로 핸들러
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    dynamic_data = get_dynamic_data()
-    return templates.TemplateResponse("index.html", {"request": request, **dynamic_data})
+suck = SocketManager
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
